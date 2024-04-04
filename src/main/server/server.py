c3ed2d7c87808c2b764c7ef06afe59a077cc512d
Bernardo Ferrão <bernardo.f@inf.ufpel.edu.br>
2024-04-04 01:26:38 -0300

    implementing get event rout

--- src/main/server/server.py
+++ src/main/server/server.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask_cors import CORS
+from RocketSeat.NLW.src.models.settings.connection import db_connection_handler
+
+db_connection_handler.connect_to_db()
 
 app = Flask(__name__)
 CORS(app)
--- src/models/repository/events_repository.py
+++ src/models/repository/events_repository.py
@@ -1,6 +1,7 @@
 from typing import Dict
 from RocketSeat.NLW.src.models.settings.connection import db_connection_handler
 from RocketSeat.NLW.src.models.entities import Events
+from RocketSeat.NLW.src.models.entities.attendees import Attendees
 from sqlalchemy.exc import IntegrityError, NoResultFound
 
 
@@ -38,4 +39,27 @@ class EventsRepository:
                 return event
             except NoResultFound:
                 return None #Caso o id não esteja, não faz nada
+
+    def count_event_attendees(self, event_id: str) -> Dict:
+        with db_connection_handler as database:
+                event_count = (
+                   database.session
+                   .query(Events)
+                    .join(Attendees, Events.id == Attendees.event_id)
+                    .filter(Events.id == event_id)
+                    .with_entities(
+                       Events.maximum_attendees,
+                       Attendees.id
+                   )
+                    .all()
+                )
+                if not len(event_count):
+                    return {
+                        "maximumAttendees": 0,
+                        "attendeesAmount": 0
+                    }
+                return {
+                    "maximumAttendees": event_count[0].maximum_attendees,
+                    "attendeesAmount": len(event_count),
+                }
 
